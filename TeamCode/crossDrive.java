0c5d16c1c0ed2e34b6dee18d2bc6b565a9ae6712
insomniac-dnd <duongnguyendavid@gmail.com>
2025-10-15 23:40:23 -0400

    Updated Code for Cross Drive Base

--- TeamCode/crossDrive.java
+++ TeamCode/crossDrive.java
@@ -15,36 +15,35 @@ public class crossDrive extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
-        // Initialize motors
+        //region Hardware Map Classes
         frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
         backLeft = hardwareMap.get(DcMotor.class, "backLeft");
         backRight = hardwareMap.get(DcMotor.class, "backRight");
 
-        // Reverse one side so all wheels move forward correctly
         frontRight.setDirection(DcMotor.Direction.REVERSE);
         backRight.setDirection(DcMotor.Direction.REVERSE);
+
+        //endregion
 
         waitForStart();
 
         while (opModeIsActive()) {
 
-            // Get joystick values
-            double y = -gamepad1.left_stick_y;    // Forward/Backward
-            double x = gamepad1.left_stick_x;     // Strafing (left/right)
-            double rx = -gamepad1.right_stick_x;   // Rotation (turn)
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x;
+            double rx = -gamepad1.right_stick_x;
 
-            // Calculate motor powers
             double frontLeftPower  = y + x + rx;
             double backLeftPower   = y - x + rx;
             double frontRightPower = y - x - rx;
             double backRightPower  = y + x - rx;
 
-            // Normalize powers so no value exceeds 1.0
             double max = Math.max(
-                    Math.max(Math.abs(frontLeftPower), Math.abs(backLeftPower)),
-                    Math.max(Math.abs(frontRightPower), Math.abs(backRightPower))
-            );
+                    Math.max(Math.abs(frontLeftPower),
+                            Math.abs(backLeftPower)),
+                    Math.max(Math.abs(frontRightPower),
+                            Math.abs(backRightPower)));
 
             if (max > 1.0) {
                 frontLeftPower /= max;
@@ -53,17 +52,16 @@ public class crossDrive extends LinearOpMode {
                 backRightPower  /= max;
             }
 
-            // Apply power to motors
             frontLeft.setPower(frontLeftPower);
             backLeft.setPower(backLeftPower);
             frontRight.setPower(frontRightPower);
             backRight.setPower(backRightPower);
 
-            // Telemetry
             telemetry.addData("Front Left", frontLeftPower);
             telemetry.addData("Back Left", backLeftPower);
             telemetry.addData("Front Right", frontRightPower);
             telemetry.addData("Back Right", backRightPower);
+
             telemetry.update();
 
             sleep(20);
--- TeamCode/sqrECUBot.java
+++ TeamCode/sqrECUBot.java
@@ -11,13 +11,10 @@ import com.qualcomm.robotcore.util.Range;
 @TeleOp(name = "sqrECUBot", group = "FTCROBOTCONTROLLER-JCM-SKILLS-REPO")
 public class sqrECUBot extends LinearOpMode {
     //region Hardware Declarations
-//    private Servo rotate0Servo;
     private CRServo vertical1Servo;
-    //    private Servo bin2Servo;
     private Servo pivot3Servo;
     private Servo wrist4Servo;
     private Servo claw5Servo;
-
     private DcMotor frontLeft;
     private DcMotor frontRight;
     private DcMotor backLeft;
@@ -25,8 +22,6 @@ public class sqrECUBot extends LinearOpMode {
     //endregion
 
     //region Servo Positions
-//    private double rotate0ServoPosition = 0.5;
-//    private double bin2ServoPosition = 0.5;
     private double pivot3ServoPosition = 0.5;
     private double wrist4ServoPosition = 0.5;
     private double claw5ServoPosition = 0.2;
@@ -44,9 +39,8 @@ public class sqrECUBot extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
 
         //region Hardware Map Classes
-//        rotate0Servo = hardwareMap.get(Servo.class,"rotate0Servo");
+
         vertical1Servo = hardwareMap.get(CRServo.class,"vertical1Servo");
-//        bin2Servo = hardwareMap.get(Servo.class, "bin2Servo");
         pivot3Servo = hardwareMap.get(Servo.class,"pivot3Servo");
         wrist4Servo = hardwareMap.get(Servo.class,"wrist4Servo");
         claw5Servo = hardwareMap.get(Servo.class,"claw5Servo");
@@ -56,10 +50,9 @@ public class sqrECUBot extends LinearOpMode {
         backLeft = hardwareMap.get(DcMotor.class, "backLeft");
         backRight = hardwareMap.get(DcMotor.class, "backRight");
 
-        frontLeft.setDirection(DcMotor.Direction.REVERSE);
         frontRight.setDirection(DcMotor.Direction.REVERSE);
-        backLeft.setDirection(DcMotor.Direction.REVERSE);
         backRight.setDirection(DcMotor.Direction.REVERSE);
+
         //endregion
 
         waitForStart();
@@ -70,16 +63,6 @@ public class sqrECUBot extends LinearOpMode {
             claw5Servo.setPosition(claw5ServoPosition);
 
             //region Claw Macro Movement
-//            if (gamepad1.left_bumper){ // claw left-right rotation
-//                if (gamepad1.dpad_left) {
-//                    rotate0ServoPosition -= servoincrement;
-//                    rotate0Servo.setPosition(rotate0ServoPosition);
-//                }
-//                if (gamepad1.dpad_right) {
-//                    rotate0ServoPosition += servoincrement;
-//                    rotate0Servo.setPosition(rotate0ServoPosition);
-//                }
-//            }
 
             if (gamepad1.y) { // raise claw
                 servopower = power1;
@@ -95,6 +78,7 @@ public class sqrECUBot extends LinearOpMode {
             //endregion
 
             //region Claw Micro Adjustments
+
             if (gamepad1.dpad_down) { // pitch down
                 pivot3ServoPosition -= servoincrement;
                 pivot3Servo.setPosition(pivot3ServoPosition);
@@ -110,35 +94,33 @@ public class sqrECUBot extends LinearOpMode {
                 wrist4ServoPosition += servoincrement;
                 wrist4Servo.setPosition(wrist4ServoPosition);
             }
+
             //endregion
 
-//            if (gamepad1.a) { // bin activation
-//                bin2ServoPosition = 1.0;
-//                bin2Servo.setPosition(bin2ServoPosition);
-//                sleep(100);
-//                bin2ServoPosition = 0.5;
-//                bin2Servo.setPosition(bin2ServoPosition);
-//            }
-
             //region Servo Range Clip
-//            rotate0ServoPosition = Range.clip(rotate0ServoPosition, servomin, servomax);
-//            bin2ServoPosition = Range.clip(bin2ServoPosition, 0.5, 1.0);
+
             pivot3ServoPosition = Range.clip(pivot3ServoPosition, servomin, servomax);
             wrist4ServoPosition = Range.clip(wrist4ServoPosition, servomin, servomax);
             claw5ServoPosition = Range.clip(claw5ServoPosition, 0.2, 0.7);
+
             //endregion
 
             //region Driving
+
             double x = (-gamepad1.left_stick_x);
             double y = (gamepad1.left_stick_y);
-            double r = (gamepad1.right_stick_x);
+            double rx = (-gamepad1.right_stick_x);
 
-//            double frontLeftPower = -0.5 * x + 0.866 * y + r;
-//            double frontRightPower = -0.5 * x - 0.866 * y + r;
-//            double backMiddlePower = x + r;
+            double frontLeftPower  = y + x + rx;
+            double backLeftPower   = y - x + rx;
+            double frontRightPower = y - x - rx;
+            double backRightPower  = y + x - rx;
 
-            double max = Math.max(Math.abs(frontLeftPower),
-                    Math.max(Math.abs(frontRightPower), Math.abs(backMiddlePower)));
+            double max = Math.max(
+                    Math.max(Math.abs(frontLeftPower),
+                            Math.abs(backLeftPower)),
+                    Math.max(Math.abs(frontRightPower),
+                            Math.abs(backRightPower)));
 
             if (max > 1.0) {
                 frontLeftPower /= max;
@@ -148,14 +130,15 @@ public class sqrECUBot extends LinearOpMode {
             }
 
             frontLeft.setPower(frontLeftPower);
+            backLeft.setPower(backLeftPower);
             frontRight.setPower(frontRightPower);
-            backMiddle.setPower(backMiddlePower);
+            backRight.setPower(backRightPower);
+
             //endregion
 
             //region Telemetry
-//            telemetry.addData("rotation0Servo:", rotate0ServoPosition);
+
             telemetry.addData("vertical1Servo:", servopower);
-//            telemetry.addData ("bin2Servo:", bin2ServoPosition);
             telemetry.addData("pivot3Servo:", pivot3ServoPosition);
             telemetry.addData("wrist4Servo:", wrist4ServoPosition);
             telemetry.addData("claw5Servo:", claw5ServoPosition);
@@ -166,6 +149,7 @@ public class sqrECUBot extends LinearOpMode {
             telemetry.addData("Back Right Power", backRighttPower);
 
             telemetry.update();
+
             //endregion
 
             sleep(20);
